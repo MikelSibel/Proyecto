1e379789f6186207a9ee54c20a370bd7b9af65a7
MikelSibel <114738354+MikelSibel@users.noreply.github.com>
2024-06-13 07:48:43 +0200

    Delete PHP directory

--- PHP/cierraSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'sesion.php';
-comprobar_sesion();
-$_SESSION = array();
-session_destroy();
-setcookie(session_name(), '', time() - 3600);
-header("Location: /Proyecto/PHP/iniciaSesion.php");
-exit;
-?>
--- PHP/conexion.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "Proyecto") or die("Imposible conectar a la base de datos");
-mysqli_set_charset($conexion, "utf8");
-?>
