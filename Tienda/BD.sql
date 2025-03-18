70c8560f447bf79b89d956fa30e42a5d4aca56cf
MikelSibel <114738354+MikelSibel@users.noreply.github.com>
2025-03-18 22:32:02 +0100

    Add files via upload

--- Tienda/BD.sql
+++ Tienda/BD.sql
@@ -19,9 +19,11 @@ CREATE TABLE IF NOT EXISTS USUARIO(
 CREATE TABLE IF NOT EXISTS PEDIDOS(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nombre VARCHAR(20) NOT NULL,
-    Estado ENUM('almacen','paqueteria', 'entregado'),
+    Estado ENUM('Almacen','Paqueteria', 'Entregado'),
     Fecha DATE NOT NULL,
-    Descripcion VARCHAR(50)
+    Descripcion VARCHAR(50),
+    Email_Usuario VARCHAR(50) NOT NULL,
+    FOREIGN KEY(Email_Usuario) REFERENCES USUARIO(Email)
 );
 
 CREATE TABLE IF NOT EXISTS VALORACIONES(
@@ -49,3 +51,15 @@ CREATE TABLE IF NOT EXISTS STOCK(
     ID_Producto INT NOT NULL,
     FOREIGN KEY(ID_Producto) REFERENCES PRODUCTO(ID_Producto)
 );
+
+INSERT INTO USUARIO(Email, Nombre, Apellido, Fecha_naci, Es_Admin)
+VALUES ('correo@admin.com', 'Soy', 'Admin', '2000', '1');
+INSERT INTO USUARIO(Email, Nombre, Apellido, Fecha_naci, Es_Admin)
+VALUES ('correo@cliente.com', 'timmy', 'timmyton', '2001', '0');
+
+INSERT INTO PEDIDOS(Nombre, Estado, Fecha, Descripcion, Email_Usuario)
+VALUES ('Pedido prueba 1','Almacen', '2025-03-18', 'pedido de prueba 1', 'correo@cliente.com')
+INSERT INTO PEDIDOS(Nombre, Estado, Fecha, Descripcion, Email_Usuario)
+VALUES ('Pedido prueba 2','Paqueteria', '2025-03-18', 'pedido de prueba 2', 'correo@cliente.com')
+INSERT INTO PEDIDOS(Nombre, Estado, Fecha, Descripcion, Email_Usuario)
+VALUES ('Pedido prueba 3','Entregado', '2025-03-18', 'pedido de prueba 3', 'correo@cliente.com')
