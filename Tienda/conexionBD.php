388f91738397cabc6e6aa080bfe1ca73f40ca9ea
MikelSibel <114738354+MikelSibel@users.noreply.github.com>
2025-05-30 00:41:57 +0200

    Delete Tienda/conexionBD.php

--- Tienda/conexionBD.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "oro") or die("No se ha conectado a la base de datos, comprueba conexionBD.php");
-mysqli_set_charset($conexion, "utf8");
-?>
